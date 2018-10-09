1c47ac84fa6f655618afbf4f54758111e4a76590
louiey <louiey@magenic.com>
2018-10-09 19:56:58 +0800

    Added update and delete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,8 +20,33 @@ class ProjectsController < ApplicationController
 
     end
   end
+  def show
+    @project = Project.find(params[:id])
+  end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if  @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to projects_path
+
+    else
+      flash[:danger] = "An error occurred."
+      render "edit"
+    end
+    end
 
 
   private
