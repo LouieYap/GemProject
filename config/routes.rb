061b6d5b6c5144063b4a5b402e4d98a53cc2c600
louiey <louiey@magenic.com>
2018-10-09 14:01:34 +0800

    Added Project create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get '/about', to: 'about#index'
   devise_for :users
+  resources :projects
+  get '/projects', to: 'projects#index'
+  get '/about', to: 'about#index'
   get '/dashboard', to: 'dashboard#index'
   root 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
