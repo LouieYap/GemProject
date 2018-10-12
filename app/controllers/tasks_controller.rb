da26faa3aa99b7987680d246fc23d60402b7dd78
ronyanf <ronyanf@magenic.com>
2018-10-12 18:16:20 +0800

    Re-commit missed files

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,28 @@ class TasksController < ApplicationController
     redirect_to dashboard_path
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if  @task.update(task_params)
+      flash[:notice] = "Task has been updated."
+      redirect_to task_path
+
+    else
+      flash[:danger] = "An error occurred."
+      render "edit"
+    end
+
+  end
+
   private
   def task_params
     params.require(:task).permit(:name, :description, :project_id, :category, :status, :priority)
