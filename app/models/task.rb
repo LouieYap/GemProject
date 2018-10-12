76c7ad3804fc3461cc2d7f61097deb02736cbdb2
louiey <louiey@magenic.com>
2018-10-12 20:50:45 +0800

    Added Tasks list and delete

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,13 @@ class Task < ApplicationRecord
   belongs_to :assigned_to, class_name: "User", optional: true
   belongs_to :created_by, class_name: "User"
   has_many :comments
+
+  scope :order_by_priority, -> {
+    order_by = ['CASE']
+    PRIORITY_ORDERS.each_with_index do |priority, index|
+      order_by << "WHEN tasks.priority='#{priority}' THEN #{index}"
+    end
+    order_by << 'END'
+    order(order_by.join(' '))
+  }
 end
