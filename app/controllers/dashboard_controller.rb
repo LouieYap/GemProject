171691dca044fbdc452bd2eb02ffad1e0765812b
louiey <louiey@magenic.com>
2018-10-16 21:02:26 +0800

    Added flash now message if theres no task assigned

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,8 @@ class DashboardController < SecureController
   def index
     @tasks = Task.order_by_priority.where(assigned_to_id: "#{current_user.id}")
 
+    if @tasks.size == 0
+      flash.now[:notice]=  "No task currently assigned to me"
+    end
   end
 end
