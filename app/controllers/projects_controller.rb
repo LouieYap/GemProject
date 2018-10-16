9a4f4d0a754f8e87b5d1164e963e56381f886ab0
louiey <louiey@magenic.com>
2018-10-16 20:50:54 +0800

    Fixed project show issue

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < SecureController
-  before_action :set_project, except: [:index, :create, :new, :show]
+  before_action :set_project, except: [:index, :create, :new]
 
   def index
   @projects = Project.all
