5dff2dd37982fb0cffb35cfd9ee8de66395ff81c
ronyanf <ronyanf@magenic.com>
2018-10-18 23:50:21 +0800

    Working save. For further refinement

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -3,9 +3,23 @@ class CommentController < SecureController
 
   end
   def create
-    @comment = Comment.new
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user
+
+    if @comment.save
+      flash[:notice] = "Comment has been created."
+      redirect_to controller:'tasks', action: 'show', id:@comment.task_id
+    else
+      flash[:danger] = "An error occurred."
+      redirect_to controller:'tasks', action: 'show', id:@comment.task_id
+    end
+
   end
   def update
 
+  end
+
+  def comment_params
+    params.require(:comment).permit(:description, :task_id)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :task
-  belongs_to :user
+  # TODO: Should not be optional
+  belongs_to :user, optional: true
 end
