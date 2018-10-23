8e22d94edfc5bf3c61c48241a5512dde32223175
louiey <louiey@magenic.com>
2018-10-23 13:39:15 +0800

    changes

--- app/jobs/send_notifications_job.rb
+++ app/jobs/send_notifications_job.rb
@@ -4,6 +4,7 @@ class SendNotificationsJob < ApplicationJob
   def perform(*args)
     puts("Sending notifications......")
     Notification.all.each do |n|
+      puts n.recipient
       CommentNotificationMailer.send_notification_mail(n.recipient).deliver_now
       n.sent_at = DateTime.now
       n.update
